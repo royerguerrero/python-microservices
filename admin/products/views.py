e5929abe85202463dad9823d8d01e398c19ccf54
Royer G <royjuni3431@gmail.com>
2021-04-19 23:46:18 -0500

    feat: add docker development enviroment, add product model, serializer and viewset(only list is working)

---
+++ admin/products/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductsSerializer
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductsSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    def create(self, request, pk=None):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
